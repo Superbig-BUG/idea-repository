ee83b912968740812872846586e9bf0cb0c36c1f
along <123456@qq.com>
2021-05-28 20:32:25 +0800

    first commit

---
+++ github-test/src/main/java/hwl/test/Test.java
@@ -0,0 +1,13 @@
+package hwl.test;
+
+/**
+ * @author along吖
+ * @create 2021-05-28 20:15
+ */
+public class Test {
+
+    @org.junit.Test
+    public void test(){
+        System.out.println("hello world");
+    }
+}
